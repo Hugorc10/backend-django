aeb9a3ef720aaf12f99c2758dae4521a78ede101
Hugo Mafra <hugorc10@gmail.com>
2024-08-30 22:45:30 -0300

    fix: :bug: change object instance of ToySerializer class.

--- restful01/toys/models.py
+++ restful01/toys/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Toy(models.Model):
-    name = models.CharField(max_length=150, blank=False, null=False, default='')
-    description = models.CharField(max_length=250, blank=False, default='')
+    name = models.CharField(max_length=150, blank=False, null=False, default="")
+    description = models.CharField(max_length=250, blank=False, default="")
     release_date = models.DateTimeField()
-    toy_category = models.CharField(max_length=200, default='')
+    toy_category = models.CharField(max_length=200, default="")
     was_included_in_home = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ('name',)
+        ordering = ("name",)
 
     def __str__(self):
         return self.name
--- restful01/toys/serializers.py
+++ restful01/toys/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Toy
 
-class ToySerializer(serializers.ModelSerializer):
+class ToySerializer(serializers.Serializer):
     pk = serializers.IntegerField(read_only=True)
     name = serializers.CharField(required=True, max_length=150)
     description = serializers.CharField(max_length=250, required=False)
@@ -13,11 +13,17 @@ class ToySerializer(serializers.ModelSerializer):
         return Toy.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.release_date = validated_data.get('release_date', instance.release_date)
-        instance.toy_category = validated_data.get('toy_category', instance.toy_category)
-        instance.was_included_in_home = validated_data.get('was_included_in_home', instance.was_included_in_home)
+        instance.name = validated_data.get("name", instance.name)
+        instance.description = validated_data.get("description", instance.description)
+        instance.release_date = validated_data.get(
+            "release_date", instance.release_date
+        )
+        instance.toy_category = validated_data.get(
+            "toy_category", instance.toy_category
+        )
+        instance.was_included_in_home = validated_data.get(
+            "was_included_in_home", instance.was_included_in_home
+        )
         instance.save()
         return instance
 
